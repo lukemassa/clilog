c501eb7daca8831bb867ad1bdadfafc6036145c9
Luke Massa <lmassa@tripadvisor.com>
2025-06-22 21:38:54 -0400

    Add doc strings

--- clilog.go
+++ clilog.go
@@ -43,23 +43,37 @@ func SetFormat(format string) error {
 
 // --- Public logging helpers ---
 
-func Debug(args ...any)                 { globalLogger.logf(LevelDebug, fmt.Sprint(args...)) }
+// Debug logs a message at DEBUG level using fmt.Sprint.
+func Debug(args ...any) { globalLogger.logf(LevelDebug, fmt.Sprint(args...)) }
+
+// Debugf logs a formatted message at DEBUG level using fmt.Sprintf.
 func Debugf(format string, args ...any) { globalLogger.logf(LevelDebug, fmt.Sprintf(format, args...)) }
 
-func Info(args ...any)                 { globalLogger.logf(LevelInfo, fmt.Sprint(args...)) }
+// Info logs a message at INFO level using fmt.Sprint.
+func Info(args ...any) { globalLogger.logf(LevelInfo, fmt.Sprint(args...)) }
+
+// Infof logs a formatted message at INFO level using fmt.Sprintf.
 func Infof(format string, args ...any) { globalLogger.logf(LevelInfo, fmt.Sprintf(format, args...)) }
 
-func Warn(args ...any)                 { globalLogger.logf(LevelWarn, fmt.Sprint(args...)) }
+// Warn logs a message at WARN level using fmt.Sprint.
+func Warn(args ...any) { globalLogger.logf(LevelWarn, fmt.Sprint(args...)) }
+
+// Warnf logs a formatted message at WARN level using fmt.Sprintf.
 func Warnf(format string, args ...any) { globalLogger.logf(LevelWarn, fmt.Sprintf(format, args...)) }
 
-func Error(args ...any)                 { globalLogger.logf(LevelError, fmt.Sprint(args...)) }
+// Error logs a message at ERROR level using fmt.Sprint.
+func Error(args ...any) { globalLogger.logf(LevelError, fmt.Sprint(args...)) }
+
+// Errorf logs a formatted message at ERROR level using fmt.Sprintf.
 func Errorf(format string, args ...any) { globalLogger.logf(LevelError, fmt.Sprintf(format, args...)) }
 
+// Fatal logs a message at FATAL level using fmt.Sprint and then exits with status code 1.
 func Fatal(args ...any) {
 	globalLogger.logf(LevelFatal, fmt.Sprint(args...))
 	os.Exit(1)
 }
 
+// Fatalf logs a formatted message at FATAL level using fmt.Sprintf and then exits with status code 1.
 func Fatalf(format string, args ...any) {
 	globalLogger.logf(LevelFatal, fmt.Sprintf(format, args...))
 	os.Exit(1)
